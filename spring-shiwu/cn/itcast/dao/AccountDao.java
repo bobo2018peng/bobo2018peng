f1091e6f68f861f01298116d65f9092bf4c41d90
bobo <bobo2018peng@163.com>
2018-09-23 00:58:49 +0800

    事务
    转账

---
+++ spring-shiwu/cn/itcast/dao/AccountDao.java
@@ -0,0 +1,6 @@
+package cn.itcast.dao;
+
+public interface AccountDao {
+	void increaseMoney(Integer id,Double money);
+	void decreaseMoney(Integer id,Double money);
+}
---
+++ spring-shiwu/cn/itcast/service/AccountService.java
@@ -0,0 +1,6 @@
+package cn.itcast.service;
+
+public interface AccountService {
+	void transfer(Integer from,Integer to,Double money);
+	
+}
